be816a6ad2e844adabe3b43a61e87f9d3868bda9
Cyril Narducci <cyril.narducci@gmail.com>
2023-05-26 16:23:26 +0200

    Commit du 26.05.2023

--- Views/php/statistiques.php
+++ Views/php/statistiques.php
@@ -1,117 +1,76 @@
-<?php
-include('../module/dbconnect.php');
-
-$currentDate = date('Y-m-d'); // Obtenez la date actuelle
-
-$startDate = date('Y-01-01');
-
-$endDate = date('Y-12-d', strtotime('+0 years', strtotime($currentDate)));
-
-$currentDate = $startDate;
-
-while ($currentDate <= $endDate) {
-  $nextDate = date('Y-m-d', strtotime('+3 months', strtotime($currentDate)));
-
-  // Graphique
-
-  $currentDate = $nextDate;
-}
-?>
-
-
+<!DOCTYPE html>
 <html>
-  <head>
+
+<head>
+</head>
+
+<body>
+    <?php
+    include('../module/dbconnect.php');
+    // Variable PHP initiale
+    $startDate = '';
+
+    // Vérifier si une nouvelle valeur a été envoyée via le formulaire
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['option'])) {
+            // Mettre à jour la variable PHP avec la nouvelle valeur
+            $startDate = $_POST['option'];
+        }
+    }
+    ?>
+
+    <form method="POST">
+        <input type="radio" name="option" value="<?php echo date('Y-01-01'); ?>" onchange="this.form.submit()"> Jan-Mar<br>
+        <input type="radio" name="option" value="<?php echo date('Y-04-01'); ?>" onchange="this.form.submit()"> Avr-Juin<br>
+        <input type="radio" name="option" value="<?php echo date('Y-07-01'); ?>" onchange="this.form.submit()"> Jui-Sep<br>
+        <input type="radio" name="option" value="<?php echo date('Y-10-01'); ?>" onchange="this.form.submit()"> Oct-Dec<br>
+    </form>
+    <?php
+    $nextDate = date('Y-m-d', strtotime('+3 months', strtotime($startDate)));
+    ?>
+
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
 
-      // Load Charts and the corechart package.
-      google.charts.load('current', {'packages':['corechart']});
+        function drawChart() {
 
-      // Callback that draws a pie chart.
-      function drawChart(data, options, chartDivId) {
-        var dataTable = new google.visualization.DataTable();
-        dataTable.addColumn('string', 'Topping');
-        dataTable.addColumn('number', 'Slices');
-        dataTable.addRows(data);
+            var data = google.visualization.arrayToDataTable([
 
-        var chart = new google.visualization.PieChart(document.getElementById(chartDivId));
-        chart.draw(dataTable, options);
-      }
+                <?php
+                $queryBikeOn = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startDate' AND bikDate < '$nextDate' AND bikState = 1");
+                $bikeOnCount = $queryBikeOn->fetch_row()[0];
 
-      // Function to generate charts
-      function generateCharts() {
-        var trim1Data = [
-          ['Mushrooms', 1],
-          ['Onions', 1],
-          ['Olives', 2],
-          ['Zucchini', 2],
-          ['Pepperoni', 1]
-        ];
-        var trim1Options = {
-          title: 'trim1',
-          width: 400,
-          height: 300
-        };
-        drawChart(trim1Data, trim1Options, 'trim1');
+                $queryBikeoFF = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startDate' AND bikDate < '$nextDate' AND bikState = 0");
+                $bikeOffCount = $queryBikeoFF->fetch_row()[0];
+                ?>
 
-        var trim2Data = [
-          ['Mushrooms', 2],
-          ['Onions', 2],
-          ['Olives', 2],
-          ['Zucchini', 0],
-          ['Pepperoni', 3]
-        ];
-        var trim2Options = {
-          title: 'trim2',
-          width: 400,
-          height: 300
-        };
-        drawChart(trim2Data, trim2Options, 'trim2');
-        var trim3Data = [
-          ['Mushrooms', 2],
-          ['Onions', 2],
-          ['Olives', 2],
-          ['Zucchini', 0],
-          ['Pepperoni', 3]
-        ];
-        var trim3Options = {
-          title: 'trim4',
-          width: 400,
-          height: 300
-        };
-        drawChart(trim3Data, trim2Options, 'trim3');
-      
+                ['String', 'Num'],
+                ['Vélos trouvés', <?php print $bikeOnCount ?>],
+                ['Vélos rendus', <?php print $bikeOffCount ?>],
 
-      var trim4Data = [
-          ['Mushrooms', 2],
-          ['Onions', 2],
-          ['Olives', 2],
-          ['Zucchini', 0],
-          ['Pepperoni', 3]
-        ];
-        var trim4Options = {
-          title: 'trim4',
-          width: 400,
-          height: 300
-        };
-        drawChart(trim4Data, trim4Options, 'trim4');
-      }
+            ]);
 
-      google.charts.setOnLoadCallback(generateCharts);
 
+            <?php
+            $startMonth = date('F', strtotime($startDate));
+            $endMonth = date('F', strtotime('-1 months', strtotime($nextDate)));
+            ?>
+            var options = {
+                title: '<?php print $startMonth . ' - ' . $endMonth ?>'
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+            chart.draw(data, options);
+        }
     </script>
-  </head>
-  <body>
-    <!--Table and divs that hold the pie charts-->
-    <table class="columns">
-      <tr>
-        <td><div id="trim1" style="border: 1px solid #ccc"></div></td>
-        <td><div id="trim2" style="border: 1px solid #ccc"></div></td>
-        <td><div id="trim3" style="border: 1px solid #ccc"></div></td>
-        <td><div id="trim4" style="border: 1px solid #ccc"></div></td>
-      </tr>
-    </table>
-  </body>
+
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+
+</body>
+
 </html>
-
-
--- Views/php/test.php
+++ Views/php/test.php
@@ -1,30 +1,117 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="FR">
 
-<style>
-  div{
-    background-color: black;
-  }
-</style>
+<head>
 
-<div id="carouselExample" class="carousel slide">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-    <h1>Graph 1</h1>
+  <!-- Meta tags -->
+  <meta charset="UTF-8" />
+  <meta name="description" content="Application Web de gestion de vélo perdu puis retrouvé. TPI-2023" />
+  <meta name="author" content="Cyril Narducci" />
+
+  <!-- Inclusion des modules de l'application -->
+  <?php
+  include('../module/islogged.php');
+  include('../module/navbar.html');
+  ?>
+
+  <!-- Titre de la page -->
+  <title>Statistiques</title>
+
+</head>
+
+<body>
+
+  <!-- Initiation du graph -->
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    // Load Charts and the corechart package.
+    google.charts.load('current', {
+      'packages': ['corechart']
+    });
+
+    // Draw the pie chart for Sarah's pizza when Charts is loaded.
+    google.charts.setOnLoadCallback(drawSarahChart);
+
+    // Draw the pie chart for the Anthony's pizza when Charts is loaded.
+    google.charts.setOnLoadCallback(drawAnthonyChart);
+
+    // Callback that draws the pie chart for Sarah's pizza.
+    function drawSarahChart() {
+
+      // Create the data table for Sarah's pizza.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 1],
+        ['Onions', 1],
+        ['Olives', 2],
+        ['Zucchini', 2],
+        ['Pepperoni', 1]
+      ]);
+
+      // Set options for Sarah's pie chart.
+      var options = {
+        title: 'How Much Pizza Sarah Ate Last Night',
+        width: 400,
+        height: 300
+      };
+
+      // Instantiate and draw the chart for Sarah's pizza.
+      var chart = new google.visualization.PieChart(document.getElementById('Sarah_chart_div'));
+      chart.draw(data, options);
+    }
+
+    // Callback that draws the pie chart for Anthony's pizza.
+    function drawAnthonyChart() {
+
+      // Create the data table for Anthony's pizza.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 2],
+        ['Onions', 2],
+        ['Olives', 2],
+        ['Zucchini', 0],
+        ['Pepperoni', 3]
+      ]);
+
+      // Set options for Anthony's pie chart.
+      var options = {
+        title: 'How Much Pizza Anthony Ate Last Night',
+        width: 400,
+        height: 300
+      };
+
+      // Instantiate and draw the chart for Anthony's pizza.
+      var chart = new google.visualization.PieChart(document.getElementById('Anthony_chart_div'));
+      chart.draw(data, options);
+    }
+  </script>
+  <!-- Fin de l'initiation -->
+
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col" id="Sarah_chart_div">
+      </div>
+      <div class="col" id="Anthony_chart_div">
+        Graph Annuel
+      </div>
     </div>
-    <div class="carousel-item">
-      <h1>Graph 2</h1>
-    </div>
-    <div class="carousel-item">
-    <h1>Graph 3</h1>
+
+    <div class="row">
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet. Ut dolore sunt 33 voluptas dolorem ea mollitia tenetur in perspiciatis beatae id ipsa voluptatem. Hic beatae sunt et cupiditate assumenda rem recusandae eveniet aut eligendi expedita aut laborum aperiam sed esse aliquam et ipsum cumque. Ut cumque ratione et nesciunt inventore et nihil galisum est maiores veritatis et soluta esse cum quia quos. Est dolor molestias quo iure laudantium sit alias dolore aut possimus quia rem molestias autem aut consequatur voluptate. Eos labore architecto in accusamus nobis sit saepe error qui omnis delectus et voluptatibus mollitia ut dolorum itaque. Vel saepe eveniet aut commodi repellendus et eligendi illo ut officia dolorem et nemo voluptatibus. Hic dolorem omnis aut porro adipisci qui accusantium incidunt sed galisum nulla ut rerum totam. </p>
+        <p>Et nobis maxime eos repudiandae quibusdam vel ratione voluptates et aliquid consectetur. 33 nulla eaque ea quas porro est similique itaque est illum fugit. Ut corrupti modi vel numquam quasi in rerum maxime aut earum consequatur et labore assumenda rem culpa voluptas. Sed libero facilis sit adipisci molestias et provident aperiam aut voluptas vitae sed nulla ipsum sit soluta delectus qui eaque necessitatibus. Ad sapiente voluptatem sed quia eveniet et quia laudantium qui aliquam saepe. Qui laborum dolores non voluptatem corrupti ut deserunt illo ut iusto inventore. Id atque rerum aut atque quisquam sed perferendis omnis in fugiat velit. Hic rerum quia id atque distinctio sit aspernatur laudantium eos consequatur facilis non tenetur saepe sed dicta quos. Non accusamus sapiente ut dolor voluptatem sed recusandae voluptates ut obcaecati sunt. </p>
+
+      </div>
+      <div class="col">
+        <p>Lorem ipsum dolor sit amet. Ut dolore sunt 33 voluptas dolorem ea mollitia tenetur in perspiciatis beatae id ipsa voluptatem. Hic beatae sunt et cupiditate assumenda rem recusandae eveniet aut eligendi expedita aut laborum aperiam sed esse aliquam et ipsum cumque. Ut cumque ratione et nesciunt inventore et nihil galisum est maiores veritatis et soluta esse cum quia quos. Est dolor molestias quo iure laudantium sit alias dolore aut possimus quia rem molestias autem aut consequatur voluptate. Eos labore architecto in accusamus nobis sit saepe error qui omnis delectus et voluptatibus mollitia ut dolorum itaque. Vel saepe eveniet aut commodi repellendus et eligendi illo ut officia dolorem et nemo voluptatibus. Hic dolorem omnis aut porro adipisci qui accusantium incidunt sed galisum nulla ut rerum totam. </p>
+        <p>Et nobis maxime eos repudiandae quibusdam vel ratione voluptates et aliquid consectetur. 33 nulla eaque ea quas porro est similique itaque est illum fugit. Ut corrupti modi vel numquam quasi in rerum maxime aut earum consequatur et labore assumenda rem culpa voluptas. Sed libero facilis sit adipisci molestias et provident aperiam aut voluptas vitae sed nulla ipsum sit soluta delectus qui eaque necessitatibus. Ad sapiente voluptatem sed quia eveniet et quia laudantium qui aliquam saepe. Qui laborum dolores non voluptatem corrupti ut deserunt illo ut iusto inventore. Id atque rerum aut atque quisquam sed perferendis omnis in fugiat velit. Hic rerum quia id atque distinctio sit aspernatur laudantium eos consequatur facilis non tenetur saepe sed dicta quos. Non accusamus sapiente ut dolor voluptatem sed recusandae voluptates ut obcaecati sunt. </p>
+
+      </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+</body>
