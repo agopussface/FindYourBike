b44e38c4b433a848cdc2921f4168612be8eeaa22
Cyril Narducci <cyril.narducci@gmail.com>
2023-05-26 16:23:00 +0200

    Commit du 26.05.2023

--- Views/php/statistiques.php
+++ Views/php/statistiques.php
@@ -1,34 +1,117 @@
 <?php
-include ('../module/dbconnect.php');
+include('../module/dbconnect.php');
+
 $currentDate = date('Y-m-d'); // Obtenez la date actuelle
 
+$startDate = date('Y-01-01');
 
-// Calculer la startDate en soustrayant 5 années à partir de la currentDate
-$startDate = date('Y-01-01', strtotime('-1 year', strtotime($currentDate)));
-
-$endDate = date('Y-12-31', strtotime('+5 years', strtotime($currentDate)));
+$endDate = date('Y-12-d', strtotime('+0 years', strtotime($currentDate)));
 
 $currentDate = $startDate;
 
 while ($currentDate <= $endDate) {
-    $nextDate = date('Y-m-d', strtotime('+3 months', strtotime($currentDate)));
+  $nextDate = date('Y-m-d', strtotime('+3 months', strtotime($currentDate)));
 
-    $startMonth = date('F', strtotime($currentDate)); // Obtenez le nom complet du mois
-    $endMonth = date('F', strtotime('-1 months', strtotime($nextDate)));
-    $year = date('Y', strtotime($currentDate)); // Obtenez le nom complet de l'année
+  // Graphique
 
-    echo $startMonth . '-' . $endMonth . ' ' . $year . "<br>"; // Afficher le mois associé à la currentDate
-
-    $query = mysqli_query($db, "SELECT * FROM t_bike WHERE bikDate >= '$currentDate' AND bikDate < '$nextDate'");
-
-    while ($row = mysqli_fetch_assoc($query)) {
-        // Afficher les données de chaque ligne
-        echo "ID : " . $row['bikFrameNo'] . "<br>";
-
-    }
-
-    $currentDate = $nextDate;
-
-    print '<br>';
+  $currentDate = $nextDate;
 }
 ?>
+
+
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+
+      // Load Charts and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Callback that draws a pie chart.
+      function drawChart(data, options, chartDivId) {
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn('string', 'Topping');
+        dataTable.addColumn('number', 'Slices');
+        dataTable.addRows(data);
+
+        var chart = new google.visualization.PieChart(document.getElementById(chartDivId));
+        chart.draw(dataTable, options);
+      }
+
+      // Function to generate charts
+      function generateCharts() {
+        var trim1Data = [
+          ['Mushrooms', 1],
+          ['Onions', 1],
+          ['Olives', 2],
+          ['Zucchini', 2],
+          ['Pepperoni', 1]
+        ];
+        var trim1Options = {
+          title: 'trim1',
+          width: 400,
+          height: 300
+        };
+        drawChart(trim1Data, trim1Options, 'trim1');
+
+        var trim2Data = [
+          ['Mushrooms', 2],
+          ['Onions', 2],
+          ['Olives', 2],
+          ['Zucchini', 0],
+          ['Pepperoni', 3]
+        ];
+        var trim2Options = {
+          title: 'trim2',
+          width: 400,
+          height: 300
+        };
+        drawChart(trim2Data, trim2Options, 'trim2');
+        var trim3Data = [
+          ['Mushrooms', 2],
+          ['Onions', 2],
+          ['Olives', 2],
+          ['Zucchini', 0],
+          ['Pepperoni', 3]
+        ];
+        var trim3Options = {
+          title: 'trim4',
+          width: 400,
+          height: 300
+        };
+        drawChart(trim3Data, trim2Options, 'trim3');
+      
+
+      var trim4Data = [
+          ['Mushrooms', 2],
+          ['Onions', 2],
+          ['Olives', 2],
+          ['Zucchini', 0],
+          ['Pepperoni', 3]
+        ];
+        var trim4Options = {
+          title: 'trim4',
+          width: 400,
+          height: 300
+        };
+        drawChart(trim4Data, trim4Options, 'trim4');
+      }
+
+      google.charts.setOnLoadCallback(generateCharts);
+
+    </script>
+  </head>
+  <body>
+    <!--Table and divs that hold the pie charts-->
+    <table class="columns">
+      <tr>
+        <td><div id="trim1" style="border: 1px solid #ccc"></div></td>
+        <td><div id="trim2" style="border: 1px solid #ccc"></div></td>
+        <td><div id="trim3" style="border: 1px solid #ccc"></div></td>
+        <td><div id="trim4" style="border: 1px solid #ccc"></div></td>
+      </tr>
+    </table>
+  </body>
+</html>
+
+
---
+++ Views/php/test.php
@@ -0,0 +1,30 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+<style>
+  div{
+    background-color: black;
+  }
+</style>
+
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <h1>Graph 1</h1>
+    </div>
+    <div class="carousel-item">
+      <h1>Graph 2</h1>
+    </div>
+    <div class="carousel-item">
+    <h1>Graph 3</h1>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
