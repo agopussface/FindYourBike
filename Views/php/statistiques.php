de56e4a086473e276f4ff4dab06f844db0387374
Cyril Narducci <cyril.narducci@gmail.com>
2023-05-31 16:24:12 +0200

    commit du 31.05.2023

--- Views/php/statistiques.php
+++ Views/php/statistiques.php
@@ -1,76 +1,157 @@
 <!DOCTYPE html>
-<html>
+<html lang="FR">
 
 <head>
+
+  <!-- Meta tags -->
+  <meta charset="UTF-8" />
+  <meta name="description" content="Application Web de gestion de vélo perdu puis retrouvé. TPI-2023" />
+  <meta name="author" content="Cyril Narducci" />
+
+  <!-- Inclusion des modules de l'application -->
+  <?php
+  include('../module/islogged.php');
+  include('../module/navbar.html');
+  include('../module/dbconnect.php');
+  ?>
+
+  <!-- Titre de la page -->
+  <title>Statistiques</title>
+
 </head>
 
 <body>
-    <?php
-    include('../module/dbconnect.php');
-    // Variable PHP initiale
-    $startDate = '';
+  <?php
 
-    // Vérifier si une nouvelle valeur a été envoyée via le formulaire
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  ?>
+  <!-- Initiation du graph -->
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+    // Load Charts and the corechart package.
+    google.charts.load('current', {
+      'packages': ['corechart']
+    });
+
+    // Draw the pie chart for Sarah's pizza when Charts is loaded.
+    google.charts.setOnLoadCallback(drawTrimChart);
+
+    // Draw the pie chart for the Anthony's pizza when Charts is loaded.
+    google.charts.setOnLoadCallback(drawAnthonyChart);
+
+    // Callback that draws the pie chart for Sarah's pizza.
+    function drawTrimChart() {
+
+      <?php
+      // Variable PHP initiale
+      $startDate = '';
+
+      // Vérifier si une nouvelle valeur a été envoyée via le formulaire
+      if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['option'])) {
-            // Mettre à jour la variable PHP avec la nouvelle valeur
-            $startDate = $_POST['option'];
+          // Mettre à jour la variable PHP avec la nouvelle valeur
+          $startDate = $_POST['option'];
         }
+      }
+
+      $nextDate = date('Y-m-d', strtotime('+3 months', strtotime($startDate)));
+
+      $queryBikeOn = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startDate' AND bikDate < '$nextDate' AND bikState = 1");
+      $bikeOnCount = $queryBikeOn->fetch_row()[0];
+
+      $queryBikeoFF = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startDate' AND bikDate < '$nextDate' AND bikState = 0");
+      $bikeOffCount = $queryBikeoFF->fetch_row()[0];
+      ?>
+
+      // Create the data table for Sarah's pizza.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'String');
+      data.addColumn('number', 'Num');
+      data.addRows([
+        ['Vélos trouvés', <?php echo $bikeOnCount; ?>],
+        ['Vélos rendus', <?php echo $bikeOffCount; ?>]
+      ]);
+      <?php
+      $startMonth = date('F', strtotime($startDate));
+      $endMonth = date('F', strtotime('-1 months', strtotime($nextDate)));
+      ?>
+
+      // Set options for Sarah's pie chart.
+      var options = {
+        title: '<?php print $startMonth . ' - ' . $endMonth; ?>',
+        width: 400,
+        height: 300
+      };
+
+      // Instantiate and draw the chart for Sarah's pizza.
+      var chart = new google.visualization.PieChart(document.getElementById('trimChart'));
+      chart.draw(data, options);
     }
-    ?>
 
-    <form method="POST">
-        <input type="radio" name="option" value="<?php echo date('Y-01-01'); ?>" onchange="this.form.submit()"> Jan-Mar<br>
-        <input type="radio" name="option" value="<?php echo date('Y-04-01'); ?>" onchange="this.form.submit()"> Avr-Juin<br>
-        <input type="radio" name="option" value="<?php echo date('Y-07-01'); ?>" onchange="this.form.submit()"> Jui-Sep<br>
-        <input type="radio" name="option" value="<?php echo date('Y-10-01'); ?>" onchange="this.form.submit()"> Oct-Dec<br>
-    </form>
-    <?php
-    $nextDate = date('Y-m-d', strtotime('+3 months', strtotime($startDate)));
-    ?>
+    // Callback that draws the pie chart for Anthony's pizza.
+    function drawAnthonyChart() {
 
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-        google.charts.load('current', {
-            'packages': ['corechart']
-        });
-        google.charts.setOnLoadCallback(drawChart);
+      // Create the data table for Anthony's pizza.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Topping');
+      data.addColumn('number', 'Slices');
+      data.addRows([
+        ['Mushrooms', 2],
+        ['Onions', 2],
+        ['Olives', 2],
+        ['Zucchini', 0],
+        ['Pepperoni', 3]
+      ]);
 
-        function drawChart() {
+      // Set options for Anthony's pie chart.
+      var options = {
+        title: 'How Much Pizza Anthony Ate Last Night',
+        width: 400,
+        height: 300
+      };
 
-            var data = google.visualization.arrayToDataTable([
-
-                <?php
-                $queryBikeOn = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startDate' AND bikDate < '$nextDate' AND bikState = 1");
-                $bikeOnCount = $queryBikeOn->fetch_row()[0];
-
-                $queryBikeoFF = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startDate' AND bikDate < '$nextDate' AND bikState = 0");
-                $bikeOffCount = $queryBikeoFF->fetch_row()[0];
-                ?>
-
-                ['String', 'Num'],
-                ['Vélos trouvés', <?php print $bikeOnCount ?>],
-                ['Vélos rendus', <?php print $bikeOffCount ?>],
-
-            ]);
+      // Instantiate and draw the chart for Anthony's pizza.
+      var chart = new google.visualization.PieChart(document.getElementById('Anthony_chart_div'));
+      chart.draw(data, options);
+    }
+  </script>
+  <!-- Fin de l'initiation -->
 
 
-            <?php
-            $startMonth = date('F', strtotime($startDate));
-            $endMonth = date('F', strtotime('-1 months', strtotime($nextDate)));
-            ?>
-            var options = {
-                title: '<?php print $startMonth . ' - ' . $endMonth ?>'
-            };
+  <div class="container text-center">
+    <div class="row">
+      <div class="col" id="trimChart">
+      </div>
+      <div class="col" id="Anthony_chart_div">
+        Graph Annuel
+      </div>
+    </div>
 
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <form method="POST">
+            <div class="btn-group" data-toggle="buttons">
+              <label class="btn btn-outline-primary">
+                <input type="radio" name="option" value="<?php echo date('Y-01-01'); ?>" onchange="this.form.submit()"> Jan-Mar
+              </label>
+              <label class="btn btn-outline-primary">
+                <input type="radio" name="option" value="<?php echo date('Y-04-01'); ?>" onchange="this.form.submit()"> Avr-Juin
+              </label>
+              <label class="btn btn-outline-primary">
+                <input type="radio" name="option" value="<?php echo date('Y-07-01'); ?>" onchange="this.form.submit()"> Jui-Sep
+              </label>
+              <label class="btn btn-outline-primary">
+                <input type="radio" name="option" value="<?php echo date('Y-10-01'); ?>" onchange="this.form.submit()"> Oct-Dec
+              </label>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="col">
 
-            chart.draw(data, options);
-        }
-    </script>
-
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
--- Views/php/test.php
+++ Views/php/test.php
@@ -1,117 +1,71 @@
-<!DOCTYPE html>
-<html lang="FR">
+<html>
 
 <head>
 
-  <!-- Meta tags -->
-  <meta charset="UTF-8" />
-  <meta name="description" content="Application Web de gestion de vélo perdu puis retrouvé. TPI-2023" />
-  <meta name="author" content="Cyril Narducci" />
-
-  <!-- Inclusion des modules de l'application -->
-  <?php
-  include('../module/islogged.php');
-  include('../module/navbar.html');
-  ?>
-
-  <!-- Titre de la page -->
-  <title>Statistiques</title>
 
 </head>
 
 <body>
+  <?php
+  include('../module/dbconnect.php');
+  // Variable PHP initiale
+  $startDate = '';
 
-  <!-- Initiation du graph -->
+  // Vérifier si une nouvelle valeur a été envoyée via le formulaire
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['option'])) {
+      // Mettre à jour la variable PHP avec la nouvelle valeur
+      $startYear = $_POST['option'];
+    }
+  }
+  ?>
+
+
+  <?php
+  $currentYear = date('Y-01-01');
+  $yearBefore = date('Y-m-d', strtotime('-1 years', strtotime($currentYear)));
+  $yearAfter = date('Y-m-d', strtotime('+1 years', strtotime($currentYear)));
+  $endOfYear = date('Y-m-d', strtotime('+1 years -1 days', strtotime($startYear)));
+  ?>
+  <form method="POST">
+    <input type="radio" name="option" value="<?php print date('Y-m-d', strtotime('-1 years', strtotime($currentYear))); ?>" onchange="this.form.submit()"> <?php print $yearBefore ?><br>
+    <input type="radio" name="option" value="<?php print date('Y-01-01'); ?>" onchange="this.form.submit()"> <?php print $currentYear ?> <br>
+    <input type="radio" name="option" value="<?php print date('Y-m-d', strtotime('+1 years', strtotime($currentYear))); ?>" onchange="this.form.submit()"> <?php print $yearAfter ?> <br>
+  </form>
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
   <script type="text/javascript">
-    // Load Charts and the corechart package.
     google.charts.load('current', {
       'packages': ['corechart']
     });
+    google.charts.setOnLoadCallback(drawChart);
 
-    // Draw the pie chart for Sarah's pizza when Charts is loaded.
-    google.charts.setOnLoadCallback(drawSarahChart);
+    function drawChart() {
 
-    // Draw the pie chart for the Anthony's pizza when Charts is loaded.
-    google.charts.setOnLoadCallback(drawAnthonyChart);
+      var data = google.visualization.arrayToDataTable([
+        <?php
+        $queryBikeOn = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startYear' AND bikDate < '$endOfYear' AND bikState = 1");
+        $bikeOnCount = $queryBikeOn->fetch_row()[0];
 
-    // Callback that draws the pie chart for Sarah's pizza.
-    function drawSarahChart() {
 
-      // Create the data table for Sarah's pizza.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Topping');
-      data.addColumn('number', 'Slices');
-      data.addRows([
-        ['Mushrooms', 1],
-        ['Onions', 1],
-        ['Olives', 2],
-        ['Zucchini', 2],
-        ['Pepperoni', 1]
+        $queryBikeoFF = mysqli_query($db, "SELECT COUNT(idBike) FROM t_bike WHERE bikDate >= '$startYear' AND bikDate < '$endOfYear' AND bikState = 0");
+        $bikeOffCount = $queryBikeoFF->fetch_row()[0];
+        ?>
+
+        ['String', 'Num'],
+        ['Vélos trouvés', <?php print $bikeOnCount ?>],
+        ['Vélos rendus', <?php print $bikeOffCount ?>],
       ]);
 
-      // Set options for Sarah's pie chart.
       var options = {
-        title: 'How Much Pizza Sarah Ate Last Night',
-        width: 400,
-        height: 300
+        title: 'My Daily Activities'
       };
 
-      // Instantiate and draw the chart for Sarah's pizza.
-      var chart = new google.visualization.PieChart(document.getElementById('Sarah_chart_div'));
-      chart.draw(data, options);
-    }
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-    // Callback that draws the pie chart for Anthony's pizza.
-    function drawAnthonyChart() {
-
-      // Create the data table for Anthony's pizza.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Topping');
-      data.addColumn('number', 'Slices');
-      data.addRows([
-        ['Mushrooms', 2],
-        ['Onions', 2],
-        ['Olives', 2],
-        ['Zucchini', 0],
-        ['Pepperoni', 3]
-      ]);
-
-      // Set options for Anthony's pie chart.
-      var options = {
-        title: 'How Much Pizza Anthony Ate Last Night',
-        width: 400,
-        height: 300
-      };
-
-      // Instantiate and draw the chart for Anthony's pizza.
-      var chart = new google.visualization.PieChart(document.getElementById('Anthony_chart_div'));
       chart.draw(data, options);
     }
   </script>
-  <!-- Fin de l'initiation -->
-
-
-  <div class="container text-center">
-    <div class="row">
-      <div class="col" id="Sarah_chart_div">
-      </div>
-      <div class="col" id="Anthony_chart_div">
-        Graph Annuel
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet. Ut dolore sunt 33 voluptas dolorem ea mollitia tenetur in perspiciatis beatae id ipsa voluptatem. Hic beatae sunt et cupiditate assumenda rem recusandae eveniet aut eligendi expedita aut laborum aperiam sed esse aliquam et ipsum cumque. Ut cumque ratione et nesciunt inventore et nihil galisum est maiores veritatis et soluta esse cum quia quos. Est dolor molestias quo iure laudantium sit alias dolore aut possimus quia rem molestias autem aut consequatur voluptate. Eos labore architecto in accusamus nobis sit saepe error qui omnis delectus et voluptatibus mollitia ut dolorum itaque. Vel saepe eveniet aut commodi repellendus et eligendi illo ut officia dolorem et nemo voluptatibus. Hic dolorem omnis aut porro adipisci qui accusantium incidunt sed galisum nulla ut rerum totam. </p>
-        <p>Et nobis maxime eos repudiandae quibusdam vel ratione voluptates et aliquid consectetur. 33 nulla eaque ea quas porro est similique itaque est illum fugit. Ut corrupti modi vel numquam quasi in rerum maxime aut earum consequatur et labore assumenda rem culpa voluptas. Sed libero facilis sit adipisci molestias et provident aperiam aut voluptas vitae sed nulla ipsum sit soluta delectus qui eaque necessitatibus. Ad sapiente voluptatem sed quia eveniet et quia laudantium qui aliquam saepe. Qui laborum dolores non voluptatem corrupti ut deserunt illo ut iusto inventore. Id atque rerum aut atque quisquam sed perferendis omnis in fugiat velit. Hic rerum quia id atque distinctio sit aspernatur laudantium eos consequatur facilis non tenetur saepe sed dicta quos. Non accusamus sapiente ut dolor voluptatem sed recusandae voluptates ut obcaecati sunt. </p>
-
-      </div>
-      <div class="col">
-        <p>Lorem ipsum dolor sit amet. Ut dolore sunt 33 voluptas dolorem ea mollitia tenetur in perspiciatis beatae id ipsa voluptatem. Hic beatae sunt et cupiditate assumenda rem recusandae eveniet aut eligendi expedita aut laborum aperiam sed esse aliquam et ipsum cumque. Ut cumque ratione et nesciunt inventore et nihil galisum est maiores veritatis et soluta esse cum quia quos. Est dolor molestias quo iure laudantium sit alias dolore aut possimus quia rem molestias autem aut consequatur voluptate. Eos labore architecto in accusamus nobis sit saepe error qui omnis delectus et voluptatibus mollitia ut dolorum itaque. Vel saepe eveniet aut commodi repellendus et eligendi illo ut officia dolorem et nemo voluptatibus. Hic dolorem omnis aut porro adipisci qui accusantium incidunt sed galisum nulla ut rerum totam. </p>
-        <p>Et nobis maxime eos repudiandae quibusdam vel ratione voluptates et aliquid consectetur. 33 nulla eaque ea quas porro est similique itaque est illum fugit. Ut corrupti modi vel numquam quasi in rerum maxime aut earum consequatur et labore assumenda rem culpa voluptas. Sed libero facilis sit adipisci molestias et provident aperiam aut voluptas vitae sed nulla ipsum sit soluta delectus qui eaque necessitatibus. Ad sapiente voluptatem sed quia eveniet et quia laudantium qui aliquam saepe. Qui laborum dolores non voluptatem corrupti ut deserunt illo ut iusto inventore. Id atque rerum aut atque quisquam sed perferendis omnis in fugiat velit. Hic rerum quia id atque distinctio sit aspernatur laudantium eos consequatur facilis non tenetur saepe sed dicta quos. Non accusamus sapiente ut dolor voluptatem sed recusandae voluptates ut obcaecati sunt. </p>
-
-      </div>
-    </div>
-  </div>
+  <div id="piechart" style="width: 900px; height: 500px;"></div>
 </body>
+
+</html>
